59e424c82bf931a98334c46f31f7c1d65e6f9adf
kaun-vaidish <vaidishthosar@gmail.com>
2023-09-24 10:51:38 +0530

    plan your trip with alert

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Explore from "./components/Explore/Explore";
 
 import Planyourtrip from "./components/planyourtrip/plan-your-trip";
 import Chooselocations from "./components/chooselocations/chooselocations";
-
-
+// import selectedPlaces from "./components/chooselocations/chooselocations";
+// import selectedDuration from "./components/planyourtrip/plan-your-trip"
 
 const App = () => {
   return (
     <>
-    
+      {/* <div>${selectedDuration}</div>
+      <div>${selectedPlaces}</div> */}
+
       <Landingpage/>
       <Explore/>
 
+      
       <Planyourtrip />
       <Chooselocations/>
       
--- src/components/planyourtrip/plan-your-trip.js
+++ src/components/planyourtrip/plan-your-trip.js
@@ -2,8 +2,6 @@ import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import { Button, Typography } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import { ClassNames } from "@emotion/react";
 
 const allowedOptions = [
   "Pune",
@@ -28,37 +26,46 @@ const durations = [
 
 const containerStyle = {
   display: "flex",
-  alignItems: "center", 
+  alignItems: "center",
   padding: "20px 100px",
 };
 
 const autocompleteStyle = {
-  flex: 1, 
-  marginRight: "16px", 
+  flex: 1,
+  marginRight: "16px",
   padding: "20px",
 };
 
 const buttonStyle = {
-  marginLeft: "auto" ,
-  
+  marginLeft: "auto",
 };
 
 const textStyle = {
-  marginLeft:"120px", 
+  marginLeft: "120px",
   padding: "5px",
   alignItems: "center",
-  
-}
-
-
+};
 
 export default function Planyourtrip() {
+  const [selectedDuration, setSelectedDuration] = React.useState(""); // State to store the selected duration
+
+  const handleSearchClick = () => {
+    
+    if (selectedDuration) {
+      alert(`You have selected Pune for your ${selectedDuration} days trip`);
+    } else {
+      alert("Please select a duration before searching.");
+    }
+  };
+
   return (
     <div className="PlanYourTrip">
       <div style={textStyle}>
         <Typography>
-      <h1 style={{ color: "black", fontSize: "60px" }}>Ready to plan your trip ?</h1>
-      </Typography>
+          <h1 style={{ color: "black", fontSize: "60px" }}>
+            Ready to plan your trip ?
+          </h1>
+        </Typography>
       </div>
 
       <div style={containerStyle}>
@@ -71,23 +78,37 @@ export default function Planyourtrip() {
             <TextField {...params} label="Select your destination..." />
           )}
           renderOption={(props, option) => (
-            <li {...props} style={{ color: 'black' }}>{option}</li>
+            <li {...props} style={{ color: "black" }}>
+              {option}
+            </li>
           )}
         />
         <Autocomplete
           disablePortal
           id="combo-box-demo"
           options={durations}
+          value={selectedDuration} // Set the value based on selected duration
+          onChange={(event, newValue) => setSelectedDuration(newValue)} // Update selected duration state
           sx={autocompleteStyle}
-          renderInput={(params) => <TextField {...params} label="What will be duration ?" />}
+          renderInput={(params) => (
+            <TextField {...params} label="What will be duration ?" />
+          )}
           renderOption={(props, option) => (
-            <li {...props} style={{ color: 'black' }}>{option}</li>
+            <li {...props} style={{ color: "black" }}>
+              {option}
+            </li>
           )}
         />
-        <Button variant="outlined" color="primary" style={buttonStyle}>
+        <Button
+          variant="outlined"
+          color="primary"
+          style={buttonStyle}
+          onClick={handleSearchClick}
+        >
           Search for places
         </Button>
       </div>
+     
     </div>
   );
 }
